ee673b90e698c3923d1f04ee5fb6e71c5998deb9
jiangchuan <985840977@qq.com>
2024-02-01 17:21:06 +0800

    第一个接口

--- collect-infor-java/src/main/java/com/collect/CollectInforJavaApplication.java
+++ collect-infor-java/src/main/java/com/collect/CollectInforJavaApplication.java
@@ -2,12 +2,18 @@ package com.collect;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class CollectInforJavaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CollectInforJavaApplication.class, args);
     }
-
+    @GetMapping("/")
+    public String index() {
+        return "hello world!";
+    }
 }
