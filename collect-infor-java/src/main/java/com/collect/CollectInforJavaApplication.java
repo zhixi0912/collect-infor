6a92bb4b5c1c3b326ab22a571549d13530a558b9
jiangchuan <985840977@qq.com>
2024-02-01 15:55:30 +0800

    添加java项目

---
+++ collect-infor-java/src/main/java/com/collect/CollectInforJavaApplication.java
@@ -0,0 +1,13 @@
+package com.collect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectInforJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectInforJavaApplication.class, args);
+    }
+
+}
