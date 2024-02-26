87eaf2e279eb1bf8c1c4c0c3fa1707e02ff1d90a
jiangchuan <985840977@qq.com>
2024-02-26 09:38:01 +0800

    remove

--- collect-infor-java/src/main/java/com/collect/CollectInforJavaApplication.java
+++ collect-infor-java/src/main/java/com/collect/CollectInforJavaApplication.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
