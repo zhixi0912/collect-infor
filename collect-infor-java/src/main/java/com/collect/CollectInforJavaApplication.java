529ccca972ec5128238b01aa5e5dcb0bc736b023
jiangchuan <985840977@qq.com>
2024-04-03 08:52:38 +0800

    delete data time

--- collect-infor-java/src/main/java/com/collect/CollectInforJavaApplication.java
+++ collect-infor-java/src/main/java/com/collect/CollectInforJavaApplication.java
@@ -33,6 +33,9 @@ public class CollectInforJavaApplication {
         chat user = 'tom'
         chat datatime = '2024-03-08'
         chat datatime = '2024-03-08'
+        chat datatime = '2024-03-08'
+        chat datatime = '2024-03-08'
+        chat datatime = '2024-03-08'
         return msg + num + name + user + datatime;
     }
 
