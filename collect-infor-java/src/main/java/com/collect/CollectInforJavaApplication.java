58e9aef22b79a64b41f22a34dfc465f7a093b4b8
jiangchuan <985840977@qq.com>
2024-06-17 09:18:20 +0800

    add user

--- collect-infor-java/src/main/java/com/collect/CollectInforJavaApplication.java
+++ collect-infor-java/src/main/java/com/collect/CollectInforJavaApplication.java
@@ -40,14 +40,6 @@ public class CollectInforJavaApplication {
         chat datatime = '2024-03-08'
         chat datatime = '2024-03-08'
         chat datatime = '2024-03-08'
-        chat datatime = '2024-03-08'
-        chat datatime = '2024-03-08'
-        chat datatime = '2024-03-08'
-        chat datatime = '2024-03-08'
-        chat datatime = '2024-03-08'
-        chat datatime = '2024-03-08'
-        chat datatime = '2024-03-08'
-        chat datatime = '2024-03-08'
 
         return msg + num + name + user + datatime;
     }
